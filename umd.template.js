3912e50f9aa6da9f7d7109814e494f58a0cab2bd
Spuds <spuds@spudsdesign.com>
2022-05-30 09:29:22 -0500

    ! minor formatting of things

--- umd.template.js
+++ umd.template.js
@@ -1,17 +1,25 @@
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
+(function (root, factory)
+{
+  if (typeof define === 'function' && define.amd)
+  {
     // AMD. Register as an anonymous module unless amdModuleId is set
-    define(["jquery"], function (a0) {
+    define(["jquery"], function (a0)
+    {
       return (factory(a0));
     });
-  } else if (typeof exports === 'object') {
+  }
+  else if (typeof exports === 'object')
+  {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
     module.exports = factory(require("jquery"));
-  } else {
+  }
+  else
+  {
     factory(jQuery);
   }
-}(this, function ($) {
+}(this, function ($)
+{
 <%= contents %>
 }));
